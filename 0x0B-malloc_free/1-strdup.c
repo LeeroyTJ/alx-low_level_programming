3f817a6a9da7ddf6bc0f7301c15faee0ccd064b3
LeeroyTJ <jumbethabol@gmail.com>
2023-03-13 10:46:20 -0700

    Task 1. The woman who has no imagination has no wings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,67 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strlen - count array
+ * @s: array of elements
+ * return: 1
+ */
+int _strlen(char *s)
+{
+	unsigned int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * _strcpy - copy string
+ * @src: array of elements
+ * @dest: dest array
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
+ * _strdup - returns pointer to a newly allocated space in
+ * memory which contain a copy of the string.
+ * @str: the string
+ *
+ * Return: a pointer to the duplicated string
+ */
+char *_strdup(char *str)
+{
+	char *dst;
+	unsigned int size;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	size = _strlen(str) + 1;
+	dst = (char *) malloc(sizeof(char) * size);
+
+	if (dst == 0)
+	{
+		return (NULL);
+	}
+	_strcpy(dst, str);
+	return (dst);
+}
