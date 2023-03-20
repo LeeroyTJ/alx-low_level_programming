efd27c35fd40c4d5f4e558e06dee1276655225ac
LeeroyTJ <jumbethabol@gmail.com>
2023-03-20 08:52:01 -0700

    Task 0. Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - dog details
+ * @name: name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ * Description: this struct if for a dog
+ */
+
 struct dog {
 
 	char *name;
